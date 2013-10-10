0a450eb4957c7c80f315262e33e685e9083ab3dc
ahoward <ara.t.howard@gmail.com>
2013-10-10 13:11:29 -0600

    sane and automatic serialization for both rails3 and rails4

--- lib/active_support/cache/mongoid_store.rb
+++ lib/active_support/cache/mongoid_store.rb
@@ -7,7 +7,7 @@ module ActiveSupport
       attr_reader :collection_name
 
       def initialize(options = {})
-        @collection_name = options[:collection] || :cache
+        @collection_name = options[:collection] || :rails_cache
         options[:expires_in] ||= 1.hour
         super(options)
       end
@@ -29,12 +29,84 @@ module ActiveSupport
       protected
 
       def write_entry(key, entry, options)
-        collection.find(_id: key).upsert(_id: key, value: binary_for(entry.value), expires_at: entry.expires_at.to_i, created_at: Time.now.utc.to_i)
+        data = Entry.data_for(entry)
+        expires_at = entry.expires_at.to_i
+        created_at = Time.now.utc.to_i
+
+        collection.find(_id: key).upsert(_id: key, data: data, expires_at: expires_at, created_at: created_at)
       end
 
-      def read_entry(key, options = nil)
-        document = collection.find(_id: key, expires_at: {'$gt' => Time.now.utc.to_i}).first
-        ActiveSupport::Cache::Entry.create(value_for(document['value']), document['created_at']) if document
+      def read_entry(key, options = {})
+        expires_at = Time.now.utc.to_i
+        doc        = collection.find(_id: key, expires_at: {'$gt' => expires_at}).first
+
+        if doc
+          data = doc['data'].to_s
+          value = Marshal.load(data)
+          created_at = doc['created_at'].to_f
+
+          Entry.for(value, created_at)
+        end
+      end
+
+    # this class exists to normalize between rails3 and rails4, but also to
+    # repair totally broken interfaces in rails - especially in rails3 - that
+    # result in lots of extra serialization/deserialzation in a class which is
+    # supposed to be FAST
+    #
+      class Entry < ::ActiveSupport::Cache::Entry
+        def Entry.is_rails3?
+          unless defined?(@is_rails3)
+            @is_rails3 = new(nil).instance_variable_defined?('@value')
+          end
+          @is_rails3
+        end
+
+      # extract marshaled data from a cache entry without doing unnecessary
+      # marshal round trips.  rails3 will have either a nil or pre-marshaled
+      # object whereas rails4 will have either a marshaled or un-marshaled
+      # object.  in both cases we want to avoid calling the silly 'value'
+      # accessor since this will cause a potential Marshal.load call and
+      # require us to make a subsequent Marshal.dump call which is SLOOOWWW.
+      #
+        def Entry.data_for(entry)
+          if is_rails3?
+            value = entry.instance_variable_get('@value')
+            marshaled = value.nil? ? Marshal.dump(value) : value
+          else
+            v = entry.instance_variable_get('@v')
+            marshaled = entry.send('compressed?') ? v : entry.send('compress', v)
+          end
+
+          Moped::BSON::Binary.new(:generic, marshaled.force_encoding('binary'))
+        end
+
+      # the intializer for rails' default Entry class will go ahead and
+      # perform and extraneous Marshal.dump on the object we just got from the
+      # db even though we don't need it hear.  rails3 has a factory to avoid
+      # this but rails4 does not so we just build the object we want and
+      # ensure to avoid any unnecessary calls to Marshal.dump...  sigh.
+      #
+        def Entry.for(value, created_at)
+          allocate.tap do |entry|
+            if is_rails3?
+              entry.instance_variable_set(:@value, value)
+              entry.instance_variable_set(:@created_at, created_at)
+              entry.instance_variable_set(:@compressed, false)
+            else
+              entry.instance_variable_set(:@v, value)
+              entry.instance_variable_set(:@c, false)
+            end
+          end
+        end
+
+        def value
+          Entry.is_rails3? ? @value : @v
+        end
+
+        def raw_value
+          Entry.is_rails3? ? @value : @v
+        end
       end
 
       def delete_entry(key, options = nil)
@@ -42,14 +114,6 @@ module ActiveSupport
       end
 
       private
-
-      def binary_for(value)
-        Moped::BSON::Binary.new(:generic, value.to_s.force_encoding('binary'))
-      end
-
-      def value_for(binary)
-        binary.to_s
-      end
 
       def collection
         Mongoid.session(:default)[collection_name]
